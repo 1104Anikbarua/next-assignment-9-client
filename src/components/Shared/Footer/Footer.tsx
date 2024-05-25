830111cfae612932941bc5e30f510a8fd8170ea9
Anik Barua <anikbb05@gmail.com>
2024-05-26 00:12:10 +0600

    complete footer section

--- src/components/Shared/Footer/Footer.tsx
+++ src/components/Shared/Footer/Footer.tsx
@@ -1,115 +1,115 @@
-import { Box, Container, Divider, Stack, Typography } from "@mui/material";
+import {
+  Box,
+  Container,
+  Divider,
+  Grid,
+  Stack,
+  Typography,
+} from "@mui/material";
 import Image from "next/image";
 import React from "react";
-import facebookLogo from "@/assets/social/facebook.png";
-import instagramLogo from "@/assets/social/instagram.png";
-import twitterLogo from "@/assets/social/twitter.png";
-import linkedinLogo from "@/assets/social/linkedin.png";
+import logo from "@/assets/logo/v (2).png";
 import Link from "next/link";
 const Footer = () => {
   return (
     <Container>
-      <Box sx={{ backgroundColor: "#16161d", py: 3 }}>
-        {/* services link start here */}
-        <Stack
-          direction={"row"}
-          alignItems={"center"}
-          justifyContent={"center"}
-          spacing={2}
-        >
-          <Typography color={"white"}>Consultation</Typography>
-          <Typography color={"white"}>Health Plans</Typography>
-          <Typography color={"white"}>Medicine</Typography>
-          <Typography color={"white"}>Diagonstics</Typography>
-          <Typography color={"white"}>NGOs</Typography>
-        </Stack>
-        {/* services link ends here */}
-        {/* social link start here */}
-        <Stack
-          direction={"row"}
-          alignItems={"center"}
-          justifyContent={"center"}
-          spacing={2}
-          sx={{ py: 2 }}
-        >
-          <Image
-            src={facebookLogo}
-            width={30}
-            height={30}
-            alt="facebook logo"
-          />
-          <Image
-            src={instagramLogo}
-            width={30}
-            height={30}
-            alt="instagram logo"
-          />
-          <Image
-            src={twitterLogo}
-            width={30}
-            height={30}
-            alt="twitterLogo logo"
-          />
-          <Image
-            src={linkedinLogo}
-            width={30}
-            height={30}
-            alt="linkedin logo"
-          />
-          {/* social link end here */}
-        </Stack>
-        {/* Divider start here */}
-        <Divider sx={{ border: "2px dashed #303f9f" }} />
-        {/* <div className="border-[1px] border-white border-dotted"></div> */}
-        {/* Divider ends here */}
-        <Stack
-          sx={{
-            py: 2,
-          }}
-          direction={"row"}
-          alignItems={"center"}
-          justifyContent={"space-between"}
-        >
-          <Typography
-            color={"white"}
+      <Stack
+        my={2}
+        direction={"row"}
+        spacing={6}
+        maxWidth={"100%"}
+        // justifyContent={"space-between"}
+        sx={{ justifyContent: { xs: "center", md: "space-between" } }}
+      >
+        <Box width={"100%"}>
+          <Box
             sx={{
-              flex: 1,
-              textAlign: "center",
+              display: "flex",
+              justifyContent: { xs: "start" },
+              gap: "12px",
             }}
           >
-            &copy; Amigo {new Date().getFullYear()} | All rights reserved
+            <Image src={logo} width={36} height={36} alt="website logo" />
+            <Typography
+              color={"rgb(117 117 117)"}
+              component={"h5"}
+              variant="h5"
+              fontWeight={500}
+            >
+              Amigo
+            </Typography>
+          </Box>
+          <Typography component={"p"} variant="body1" color={"#4b5563"}>
+            Turn your next trip into a hassle-free experience with Amigo
           </Typography>
-          <Typography
-            color={"white"}
-            component={Link}
-            href={"/"}
-            variant="h5"
-            sx={{
-              flex: 1,
-              textAlign: "center",
-            }}
-          >
-            Tele
-            <Box component={"span"} color={"primary.main"} ml={0.5}>
-              M
-            </Box>
-            edicine
-          </Typography>
-          <Stack
-            sx={{
-              flex: 1,
-              textAlign: "center",
-            }}
-          >
-            <Typography color={"white"} component={Link} href={"/"}>
+        </Box>
+        <Grid container sx={{ xs: 2, md: 3 }} columnGap={2}>
+          <Stack spacing={1}>
+            <Typography variant="subtitle1" component={"h6"} fontWeight={700}>
+              Legal
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{
+                cursor: "pointer",
+                "&:hover": { textDecoration: "underline" },
+              }}
+            >
+              Terms and conditions
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{
+                cursor: "pointer",
+                "&:hover": { textDecoration: "underline" },
+              }}
+            >
               Privacy Policy
             </Typography>
-            <Typography color={"white"} component={Link} href={"/"}>
-              Terms &amp; Conditions
+          </Stack>
+          <Stack spacing={1}>
+            <Typography variant="subtitle1" component={"h6"} fontWeight={700}>
+              Support
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{
+                cursor: "pointer",
+                "&:hover": { textDecoration: "underline" },
+              }}
+            >
+              Contact us
             </Typography>
           </Stack>
-        </Stack>
-      </Box>
+          <Stack spacing={1}>
+            <Typography variant="subtitle1" component={"h6"} fontWeight={700}>
+              Itineraries
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{
+                cursor: "pointer",
+                "&:hover": { textDecoration: "underline" },
+              }}
+            >
+              Community Trips
+            </Typography>
+            <Typography
+              variant="body1"
+              sx={{
+                cursor: "pointer",
+                "&:hover": { textDecoration: "underline" },
+              }}
+            >
+              Destination
+            </Typography>
+          </Stack>
+        </Grid>
+      </Stack>
+      <Divider sx={{ backgroundColor: "#757575" }} />
+      <Typography py={3} color={"#757575"} component={"h5"} variant="h5">
+        &copy; Amigo {new Date().getFullYear()} | All rights reserved
+      </Typography>
     </Container>
   );
 };
